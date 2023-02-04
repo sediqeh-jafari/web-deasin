c236519afede2adf320315f7bfe2ab8d90dae1e4
niloofar-jafari <mohammadalimalekzadah90@gmail.com>
2023-02-04 22:28:38 +0330

    feat: Initial codes added

---
+++ src/componentes/Footer/index.js
@@ -0,0 +1,18 @@
+import './style.css'
+
+
+
+function Footer(){
+    return(
+        <div className='footer'>
+          hello world
+        </div>
+    )
+      
+    
+}
+
+export default Footer
+
+
+
---
+++ src/componentes/Main/index.js
@@ -0,0 +1,29 @@
+import './style.css'
+
+
+
+function Main (){
+    return(
+        <ul className='main'>
+          <li>1کار <i class="fa fa-times  remove"></i></li>
+          <li>کار2 <i class="fa fa-times  remove"></i></li>
+          <li>کار3 <i class="fa fa-times  remove"></i></li>
+          <li>کار4 <i class="fa fa-times  remove"></i></li>
+          <li>کار5 <i class="fa fa-times  remove"></i></li>
+          <li>کار6 <i class="fa fa-times  remove"></i></li>
+          <li>کار7 <i class="fa fa-times  remove"></i></li>
+          <li>کار8 <i class="fa fa-times  remove"></i></li>
+          <li>کار9 <i class="fa fa-times  remove"></i></li>
+          <li>کار10 <i class="fa fa-times  remove"></i></li>
+          
+
+        </ul>
+    )
+      
+    
+}
+
+export default Main
+
+
+
